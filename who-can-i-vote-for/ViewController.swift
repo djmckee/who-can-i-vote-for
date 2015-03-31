3f1a4d25083c885d7377077c5ab21ce0f6f5200e
Dylan McKee <dylan@djmckee.co.uk>
2015-03-31 11:57:05 +0100

    Initial Commit

---
+++ who-can-i-vote-for/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  who-can-i-vote-for
+//
+//  Created by Dylan McKee on 31/03/2015.
+//  Copyright (c) 2015 djmckee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
