6dc3dcaffe22218cffbb6880cf7078bdaf787fa5
Dylan McKee <dylan@djmckee.co.uk>
2015-04-03 17:37:17 +0100

    Candidates

--- who-can-i-vote-for/CandidateTableViewCell.swift
+++ who-can-i-vote-for/CandidateTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class CandidateTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var nameLabel: UILabel?
     @IBOutlet weak var partyLabel: UILabel?
 
--- who-can-i-vote-for/CandidateTableViewController.swift
+++ who-can-i-vote-for/CandidateTableViewController.swift
@@ -15,6 +15,8 @@ class CandidateTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.title = "Candidates"
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
