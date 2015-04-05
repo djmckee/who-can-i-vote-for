853076339b96228de71faa1a056bb50f342a036a
David Robinson <davidrobinson@Davids-MacBook-Pro.local>
2015-04-05 15:27:29 +0100

    Additional Colours
    Added actual colours for all parties and additional parties colours,
    included if statements to switch themes

--- who-can-i-vote-for/PartyColours.swift
+++ who-can-i-vote-for/PartyColours.swift
@@ -9,33 +9,77 @@
 import Foundation
 import UIKit
 
+var theme = "flat"
+
 // some constant political party colours... (cool colours from http://flatuicolors.com/ )
 class PartyColours {
     
     
     class func colourForParty(partyName :String!) -> UIColor {        
         if partyName.contains("Labour Party") {
-            // Labour red
-            return UIColor(red: (179.0000/255.0), green: (36.0000/255.0), blue: (28.0000/255.0), alpha: (1.0000))
+            // Labour Red
+            if theme == "flat" {
+                return UIColor(red: (179.0000/255.0), green: (36.0000/255.0), blue: (28.0000/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: (178.0000/255.0), green: (34.0000/255.0), blue: (34.0000/255.0), alpha: (1.0000))
+            }
         } else if partyName.contains("Conservative Party") {
-            // Tory blue
-            return UIColor(red: (29.0000/255.0), green: (106.0000/255.0), blue: (173.0000/255.0), alpha: (1.0000))
+            // Tory Blue
+            if theme == "flat" {
+                return UIColor(red: (29.0000/255.0), green: (106.0000/255.0), blue: (173.0000/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: (0.0000/255.0), green: (135.0000/255.0), blue: (220.0000/255.0), alpha: (1.0000))
+            }
         } else if partyName.contains("Green Party") {
-            // Green green
-            return UIColor(red: (30.0000/255.0), green: (164.0000/255.0), blue: (75.0000/255.0), alpha: (1.0000))
+            // Green Party Green
+            if theme == "flat" {
+                return UIColor(red: (30.0000/255.0), green: (164.0000/255.0), blue: (75.0000/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: (106.0000/255.0), green: (176.0000/255.0), blue: (35/255.0), alpha: (1.0000))
+            }
         } else if partyName.contains("UK Independence Party"){
-            // purple.
-            return UIColor(red: (123.0000/255.0), green: (38.0000/255.0), blue: (159.0000/255.0), alpha: (1.0000))
+            // UKIP Purple
+            if theme == "flat" {
+                return UIColor(red: (123.0000/255.0), green: (38.0000/255.0), blue: (159.0000/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: (112/255), green: (20/255), blue: (122/255), alpha: 1)
+            }
         } else if partyName.contains("Scottish National Party") {
-            // A light ish yellow
-            return UIColor(red: (207.0/255.0), green: (202.0/255.0), blue: (24.0/255.0), alpha: (1.0000))
+            // SNP Yellow
+            if theme == "flat" {
+                return UIColor(red: (207.0/255.0), green: (202.0/255.0), blue: (24.0/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+            }
         } else if partyName.contains("Plaid Cymru") {
             // A darkish green.
-            return UIColor(red: (50.0000/255.0), green: (116.0000/255.0), blue: (30.0000/255.0), alpha: (1.0000))
+            if theme == "flat" {
+                return UIColor(red: (50.0000/255.0), green: (116.0000/255.0), blue: (30.0000/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: 0, green: (129/255), blue: (66/255), alpha: 1)
+            }
         } else if partyName.contains("Liberal Democrats"){
             // Lib dem yellow.
-            return UIColor(red: (238.0000/255.0), green: (187.0000/255.0), blue: (0.0000/255.0), alpha: (1.0000))
-        }else {
+            if theme == "flat" {
+                return UIColor(red: (238.0000/255.0), green: (187.0000/255.0), blue: (0.0000/255.0), alpha: (1.0000))
+            } else {
+                return UIColor(red: (253/255.0), green: (187.0000/255.0), blue: (48.0000/255.0), alpha: (1.0000))
+            }
+        } else if partyName.contains("Independent") {
+            // Indep grey
+            if theme == "flat" {
+                return UIColor(red: (149/255), green: (165/255), blue: (166/255), alpha: (1.0))
+            } else {
+                return UIColor(red: (221/255), green: (221/255), blue: (221/255), alpha: (1.0))
+            }
+        } else if partyName.contains("British National Party") {
+            // BNP Black
+            if theme == "flat" {
+                return UIColor(red: (12/255), green: (12/255), blue: (12/255), alpha: (1.0))
+            } else {
+                return UIColor(red: (1), green: (1), blue: (139/255), alpha: (1.0))
+            }
+        } else {
             // a nice neuteral turquoise-ish.
             return UIColor(red: (21.0000/255.0), green: (178.0000/255.0), blue: (138.0000/255.0), alpha: (1.0000))
         }
